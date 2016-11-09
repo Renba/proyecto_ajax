04fbbe8fb3179c10617ea51e6a2ea4b41203e370
renba <cs_abner@hotmail.com>
2016-11-09 11:01:19 -0600

    login

--- proye_ajax/app/controllers/login.php
+++ proye_ajax/app/controllers/login.php
@@ -1,26 +1,21 @@
 <?php
 session_start();
-header("Content-Type:text/plain");
 $_SESSION["valid"] = false;
 $_SESSION["email"] = "";
 
+require_once('../daos/userDao.php');
 $user_email=$_POST['email'];
 $password=$_POST['password'];
-echo($user_email);
-echo($password);
-
-require_once('../daos/userDao.php');
-$result=getCredentialsUser($user_email, $password);
 $status = "";
 if($status == ""){
-  if(true){
+  $result = getCredentialsUser($user_email, $password);
+  if($result->num_rows > 0){
     $_SESSION["valid"] = true;
     $_SESSION["email"] = $user_email;
-
-    $status = "";
+    $status = "ok";
   }else{
       $status = "no existe el usuario con ese usuario y contraseña";
   }
-echo($status);
+  echo($status);
 }
 ?>
